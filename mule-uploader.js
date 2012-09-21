46bbe2803a393b9d2c23162c67cdd3876d4064a1
Cinely <gabi@cinely.com>
2012-09-21 12:50:24 -0700

    Update mule-uploader.js

--- mule-uploader.js
+++ mule-uploader.js
@@ -1,9 +1,18 @@
-/**
- * mule-uploader.js
+/*
  *
- * Copyright 2012, Gabriel Purcaru
- * Released under GPL License.
- * License: http://www.gnu.org/copyleft/gpl.html
+ * Written by Gabriel Purcaru
+ * Copyright 2012 Cinely, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 function mule_upload(input, settings) {
