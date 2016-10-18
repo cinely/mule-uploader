064dd92ea82713dcdcc4ed086eb1c3efc548629a
Alin Purcaru <mesmerize_ro@yahoo.com>
2016-10-18 21:29:28 +0300

    Added nodejs backend, using express for http server and sqlite as a db engine.

---
+++ backends/nodejs-express/settings.js
@@ -0,0 +1,13 @@
+var settings = {
+	aws_access_key: '[your-access-key]',
+	aws_secret: '[your-secret]',
+	region: '[aws-region-id]',
+    bucket: '[s3-bucket-id]',
+
+    mime_type: 'application/octet-stream',
+	chunk_size: 6 * 1024 * 1024, //warning: if this changes the DB needs to be flushed
+
+    dbFileName: './uploads.db',
+    serverPort: 3000
+};
+module.exports = settings;
